d8c008191e800569e13b6417762f7172b823b614
David Ntwari <d.ntwari@alustudent.com>
2023-08-09 16:32:05 +0200

    true

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace TrueOrFalse
+{
+  class Program
+  {
+		static void Main(string[] args)
+    {
+      // Do not edit these lines
+      Console.WriteLine("Welcome to 'True or False?'\nPress Enter to begin:");
+      string entry = Console.ReadLine();
+      Tools.SetUpInputStream(entry);
+
+      // Type your code below
+      string[] questions = {"is lebron the best nba player?","is casablanca the best romance movie?","is citizen kane overrated?","is kanye west the best rapper?","have you fully integrated your dark-side as recommended by dr.jung?"};
+    
+    
+  	bool[] answers = {false,true, true,true,false};
+    bool[] responses = new bool [questions.Length];
+    if (questions.Length != answers.Length){
+      Console.WriteLine("Answer all questions you bastard.... please");
+    }
+
+  int askingIndex = 0;
+
+  foreach (string question in questions){
+    string input;
+    bool isBool;
+    bool inputBool;
+    Console.WriteLine(question);
+    Console.WriteLine("True or false?");
+    input = Console.ReadLine();
+    
+    isBool = Boolean.TryParse(input, out inputBool);
+
+    while (isBool == false){
+      Console.WriteLine("please respond with 'true' or 'false' you musty bitch");
+      Console.WriteLine(question);
+      Console.WriteLine("True or false?");
+      input = Console.ReadLine();
+
+      isBool = Boolean.TryParse(input, out inputBool);
+    }
+    responses[askingIndex] = inputBool;
+    askingIndex += 1;
+
+  }
+
+  int scoringIndex = 0;
+  int score = 0;
+
+  foreach (bool answer in answers){
+    bool ressy = responses[scoringIndex];
+    Console.WriteLine($"Input: {ressy} | Answer: {answer}");
+
+    if(answer == ressy){
+      score += 1;
+    }
+    scoringIndex += 1;
+
+    Console.WriteLine($"You got {score} out of 5 correct!");
+  }
+}
+      
+      }
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# true_or_false_quiz
+
+
+##TrueOrFalse
+#Introduction
+Welcome to 'True or False?' - a C# console application where users can answer true/false questions and see their score at the end. The program will present a set of predefined true/false questions to the user and prompt them to respond with either 'true' or 'false.' After all the questions have been answered, the program will calculate and display the user's score based on the correct responses.
+
+#How to Run
+Make sure you have a C# development environment set up on your computer.
+Download the source code files from this repository.
+Open the TrueOrFalse.csproj file in your C# IDE (e.g., Visual Studio or Visual Studio Code).
+Build and run the application from the IDE or using the command-line interface.
+
+#Learning
+I used this small game to better understand loops and arrays.
+
+Thank you for stoping by! Happy Coding!
